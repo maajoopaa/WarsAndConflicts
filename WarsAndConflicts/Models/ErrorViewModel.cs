5edfac635ccde6b3b600c4c92480b8e34ff1659a
Opa Maj <saintkilla79@gmail.com>
2025-04-04 17:51:33 +0300

    Добавьте файлы проекта.

---
+++ WarsAndConflicts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WarsAndConflicts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
