2465d58a00c5d14cd783814aa6699e75c37f8db1
Opa Maj <saintkilla79@gmail.com>
2025-04-05 12:00:28 +0300

    Migrations

--- WarsAndConflicts/Migrations/20250404201424_UsersRole.Designer.cs
+++ WarsAndConflicts/Migrations/20250404201424_UsersRole.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WarsAndConflicts.DataAccess;
@@ -11,9 +12,11 @@ using WarsAndConflicts.DataAccess;
 namespace WarsAndConflicts.Migrations
 {
     [DbContext(typeof(WarsDbContext))]
-    partial class WarsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250404201424_UsersRole")]
+    partial class UsersRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WarsAndConflicts/Migrations/20250404201424_UsersRole.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarsAndConflicts.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "IsAdmin",
+                table: "Users",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Users");
+        }
+    }
+}
